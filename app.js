587c2ee64b6f511b41d9b57e71bf48183fb36831
alib2004 <aliw.b2004@gmail.com>
2024-12-27 22:53:17 +0330

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let items = [
     price: 1.5,
     image: "./apple.jpg",
     quantity: 1,
-    totalprice: 1.5
+    totalprice: 1.5,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ let items = [
     price: 0.8,
     image: "./banana.jpg",
     quantity: 1,
-    totalprice: 0.8
+    totalprice: 0.8,
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ let items = [
     price: 1.2,
     image: "./orange.jpg",
     quantity: 1,
-    totalprice: 1.2
+    totalprice: 1.2,
   },
   {
     id: 4,
@@ -29,68 +29,78 @@ let items = [
     price: 2.5,
     image: "./grapes.jpg",
     quantity: 1,
-    totalprice: 2.5
+    totalprice: 2.5,
   },
 ];
 let cartBasket = [];
 let grid = document.querySelector(".grid");
 let buys = document.querySelector(".buys");
-let total = document.querySelector("h3");
+let totalprice = document.querySelector("h3");
 items.forEach((item) => {
   grid.insertAdjacentHTML(
     "beforeend",
-    `
-            <div class="col">
-                    <h2>${item.name}</h2>
-                    <img src=${item.image} alt="" width="100%">
-                    <div class="price-btn">
-                        <span>$${item.price}</span>
-                        <div>
-                            <input type="number" max="3" value="1"/>
-                            <button class="add-to-cart" id=${item.id}>add to cart</button>
-                        </div>
-                    </div>
-            </div>
-        `
+    `<div class="col"><h2>${item.name}</h2><img src=${item.image}><div class="price-btn"><span>${item.price}$</span><button class="add-to-cart" onclick="addproducttobasket(${item.id});">add to cart</button></div></div>`
   );
 });
-grid.addEventListener("click", (e) => {
-  if (e.target.classList.contains("add-to-cart")) {
-    const productCard = e.target.id;
-    let what = items.find((prod) => prod.id == productCard);
-    userAddtoCart(what);
-  }
-});
-const userAddtoCart = (prod) => {
-  if (cartBasket.includes(prod)) {
-    prod.quantity += 1;
-    prod.totalprice = prod.price * prod.quantity;
-  } else {
-    let sum = 0
-    cartBasket = []
-    cartBasket.push(prod);
-    cartBasket.forEach((item) => {
-        sum += item.totalprice
-      buys.insertAdjacentHTML(
-        "beforeend",
-        `
-              <div class="item">
-                <h2>${item.name}</h2>
-                <p>$${item.totalprice}</p>
-                <div class="btns">
-                  <input type="number" max="3" id="" />
-                  <button class="remove">remove</button>
-                </div>
-              </div>
-          `
-      );
+function addproducttobasket(id) {
+  let userselect = items.find((prod) => prod.id == id);
+  cartBasket.push(userselect);
+  basketproductgenerator(cartBasket);
+  calctotalprice(cartBasket);
+}
+function basketproductgenerator(basket) {
+  buys.innerHTML = "";
+
+  basket.forEach((item) => {
+    let items = document.createElement("div");
+    items.classList.add("item");
+
+    let h2 = document.createElement("h2");
+    h2.innerHTML = item.name;
+
+    let p = document.createElement("p");
+    p.innerHTML = item.totalprice;
+
+    let btns = document.createElement("div");
+    btns.classList.add("btns");
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = item.quantity;
+    input.max = 3;
+    input.addEventListener("change", (e) => {
+      updateprodcount(item.id, e.target.value);
     });
-  }
-  
-  total.innerHTML = `total price is : ${sum}`
-};
-buys.addEventListener("click", (e) => {
-  if (e.target.classList.contains("remove")) {
-    e.target.closest(".item").remove();
-  }
-});
+
+    let button = document.createElement("button");
+    button.classList.add("remove");
+    button.innerHTML = "remove";
+    button.addEventListener("click", () => {
+      removefromcart(item.id);
+    });
+
+    btns.append(input, button);
+    items.append(h2, p, btns);
+    buys.append(items);
+  });
+}
+function removefromcart(id) {
+  cartBasket = cartBasket.filter((item) => item.id !== id);
+  basketproductgenerator(cartBasket);
+  calctotalprice(cartBasket);
+}
+function calctotalprice(total) {
+  let sum = 0;
+  total.forEach((item) => {
+    sum += item.price * item.quantity;
+  });
+  totalprice.innerHTML = `total price is ${sum}`;
+}
+function updateprodcount(prodid, newcount) {
+  cartBasket.forEach((item) => {
+    if (item.id == prodid) {
+      item.quantity = newcount;
+    }
+  });
+  calctotalprice(cartBasket);
+}
